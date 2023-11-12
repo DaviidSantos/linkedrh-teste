578dbbf23e7526655cd47b30f4d46cbc07fb7d10
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-11-12 10:25:09 -0300

    feat: initial configuration

---
+++ script.sql
@@ -0,0 +1,30 @@
+CREATE TABLE curso(
+                      codigo SERIAL PRIMARY KEY,
+                      nome VARCHAR(100) NOT NULL,
+                      descricao VARCHAR(4000) NOT NULL,
+                      duracao INTEGER NOT NULL
+);
+
+CREATE TABLE funcionario(
+                            codigo SERIAL PRIMARY KEY,
+                            nome VARCHAR(200) NOT NULL,
+                            cpf CHAR(11) NOT NULL,
+                            nascimento DATE NOT NULL,
+                            cargo VARCHAR(200) NOT NULL,
+                            admissao DATE NOT NULL,
+                            status BIT NOT NULL
+);
+
+CREATE TABLE turma(
+                      codigo SERIAL PRIMARY KEY,
+                      inicio DATE NOT NULL,
+                      fim DATE NOT NULL,
+                      local VARCHAR(200),
+                      curso INTEGER REFERENCES curso(codigo)
+);
+
+CREATE TABLE turma_participante(
+                                   codigo SERIAL PRIMARY KEY,
+                                   turma INTEGER REFERENCES turma(codigo),
+                                   funcionario INTEGER REFERENCES funcionario(codigo)
+);
---
+++ src/test/java/com/linkedrhteste/LinkedrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedrhteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedrhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
