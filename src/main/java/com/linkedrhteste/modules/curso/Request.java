76136538f26ba42986ddf407279e2f07a004ad19
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-11-12 10:25:56 -0300

    feat: add curso request

---
+++ src/main/java/com/linkedrhteste/modules/curso/Request.java
@@ -0,0 +1,19 @@
+package com.linkedrhteste.modules.curso;
+
+public class Request {
+    private String nome;
+    private String descricao;
+    private Integer duracao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Integer getDuracao() {
+        return duracao;
+    }
+}
